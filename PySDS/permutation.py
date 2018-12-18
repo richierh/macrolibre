e3e478eb68dcb6cddda9e4ca6964b737e6c44fc2
Richie <richie130283@gmail.com>
2018-12-18 09:29:05 +0700

    aa

--- PySDS/permutation.py
+++ PySDS/permutation.py
@@ -1,3 +1,31 @@
 import itertools
-k = list(itertools.permutations(["R", "I", "A", "S", "E", "C"]))
-print (k)
+
+import csv
+
+class CustomPermutation(itertools.permutations):
+    
+    def __init__(self,*args,**kwds):
+        super(CustomPermutation,self).__init__()#,**kwds)
+        self.objectoflist=str(args[0]).upper()
+        self.permutation=itertools.permutations(self.objectoflist)
+    
+    def insert(self):
+        with open('permut.csv', 'w') as writeFile:
+            write = csv.writer(writeFile)
+            write.writerows([lista for lista in self.k])
+        writeFile.close()
+        
+    def readpermutation(self):
+        a = [lista for lista in self.permutation]
+        self.k = []
+        for j in a:
+            self.k.append(["".join(j)])
+        return self.k
+    
+ 
+if __name__=="__main__":
+    itter = ("r","e","c")
+    run = CustomPermutation(itter)
+    run.readpermutation()
+    run.insert()
+
